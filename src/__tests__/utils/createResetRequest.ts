c24e4b14cf771547501a0cc8de4cfaf5c9e28589
Oosasukel <rodrigo_gonn@hotmail.com>
2021-07-16 01:57:54 -0300

    Fix tests

---
+++ src/__tests__/utils/createResetRequest.ts
@@ -0,0 +1,26 @@
+import { Connection } from 'typeorm';
+import PasswordResetRequest from '../../models/PasswordResetRequest';
+import User from '../../models/User';
+
+const createUser = async (connection: Connection) => {
+  const request_secret = 'secret';
+  const usersRepository = connection.getRepository(User);
+  const user = usersRepository.create({
+    nickname: 'nickname',
+    email: 'email@example.com',
+    password: '123',
+  });
+  await usersRepository.save(user);
+
+  const resetRequestsRepository =
+    connection.getRepository(PasswordResetRequest);
+  const resetRequest = resetRequestsRepository.create({
+    user_id: user.id,
+    request_secret,
+  });
+  await resetRequestsRepository.save(resetRequest);
+
+  return { requestId: resetRequest.id, request_secret };
+};
+
+export default createUser;
