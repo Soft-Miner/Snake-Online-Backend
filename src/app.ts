2f0bd5643773090681e1bd07093f894d18e1d30f
Oosasukel <rodrigo_gonn@hotmail.com>
2021-10-04 22:41:40 -0300

    Colyseus Config

--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,14 @@
-import { monitor } from '@colyseus/monitor';
-import { WebSocketTransport } from '@colyseus/ws-transport';
-import { Server } from 'colyseus';
 import cors from 'cors';
 import express from 'express';
 import 'express-async-errors';
 import 'reflect-metadata';
 import swaggerUi from 'swagger-ui-express';
-import { configureColyseus } from './colyseus';
 import { appError } from './middlewares/appError';
 import routes from './routes';
 import swaggerDocs from './swagger.json';
 
-const createServer = (
-  process.env.NODE_ENV === 'development' ? require('http') : require('https')
-).createServer;
-
 const app = express();
 
-app.use(
-  '/colyseus',
-  monitor({
-    columns: [
-      'roomId',
-      'name',
-      'clients',
-      { metadata: 'name' },
-      { metadata: 'playing' },
-      { metadata: 'mapSize' },
-      { metadata: 'maxClients' },
-      'locked',
-      'elapsedTime',
-    ],
-  })
-);
 app.use(express.json());
 app.use(cors());
 
@@ -41,12 +17,4 @@ app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use('/api', routes);
 app.use(appError());
 
-const gameServer = new Server({
-  transport: new WebSocketTransport({
-    server: createServer(app),
-  }),
-});
-
-configureColyseus(gameServer);
-
-export default gameServer;
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -6,11 +6,14 @@ if (process.env.NODE_ENV === 'development') {
 
 import { createConnection } from 'typeorm';
 import app from './app';
+import { configureColyseus } from './colyseus';
 
 createConnection();
 
 const PORT = Number(process.env.PORT) || 3333;
 
-app.listen(PORT, undefined, undefined, () => {
+const gameServer = configureColyseus(app);
+
+gameServer.listen(PORT, undefined, undefined, () => {
   console.log(`⚡️ Listening at http://localhost:${PORT}`);
 });
